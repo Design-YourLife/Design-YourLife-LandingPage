50081e9ae315297d50b07b39c7a49031ff63d98d
Jessica <Jessica@users-iMac.local>
2019-10-23 23:30:54 -0400

    menu bar active on index.html

--- modal.js
+++ modal.js
@@ -5,3 +5,17 @@ document.getElementById('btn_started').addEventListener('click', function() {
 document.querySelector('.close').addEventListener('click', function() {
   document.querySelector('.bg-modal').style.display = 'none';
 });
+
+/////////////////////////////////////////
+
+document.getElementById('menu-icon').addEventListener('click', function() {
+  document.querySelector('.menu-modal').style.display = 'flex';
+  document.querySelector('.menu_off').style.display = 'none';
+  document.querySelector('.menu_on').style.display = 'flex';
+});
+
+document.getElementById('menu-icon2').addEventListener('click', function() {
+  document.querySelector('.menu-modal').style.display = 'none';
+  document.querySelector('.menu_off').style.display = 'flex';
+  document.querySelector('.menu_on').style.display = 'none';
+});
